8408201860750b3572b092fbd1b3278e0823adbd
Vimmy <me@vimarshraina.com>
2016-11-21 18:18:41 +0000

    Minor refactor

--- callback_handler.ts
+++ callback_handler.ts
@@ -28,6 +28,16 @@ export class CallbackHandler {
         return this.slack_rtm.dataStore.getChannelGroupOrDMById(id);
     }
 
+    private CheckSplitSend(arg : boolean, message1 : string, message2 : string) : void {
+
+        if (arg) {
+            this.SendMessageToChannel(message1);
+        }
+        else {
+            this.SendMessageToChannel(message2);
+        }
+    }
+
     SetCurrentMessage(_current_message : any) : void {
         this.current_message = _current_message;
     }
@@ -38,60 +48,43 @@ export class CallbackHandler {
 
     OnGroupAdd(args : string[], arg_pos : number) : void {
         let group_name = args[arg_pos + 1];
-        let message : string;
-        
-        if (!this.group_manager.CreateGroup(group_name)) {
-            message = this.PRE_FORMAT + "Group *[" + group_name + "]*" + " already exists";
-        }
-        else {
-            message = this.PRE_FORMAT + "Group *[" + group_name + "]*" + " created successfully";
-        }
 
-        this.SendMessageToChannel(message);
+        this.CheckSplitSend(
+            !this.group_manager.CreateGroup(group_name),
+            this.PRE_FORMAT + "Group *[" + group_name + "]*" + " already exists",
+            this.PRE_FORMAT + "Group *[" + group_name + "]*" + " created successfully"
+        );
     }
 
     OnGroupList(args : string[], arg_pos : number) : void {
         let all_groups = this.group_manager.GetAllGroups();
-        let message : string;
 
-        if (all_groups.length <= 0) {
-            message = this.PRE_FORMAT + "No Groups exists yet";
-        }
-        else {
-            message = this.PRE_FORMAT + "Groups are: " + all_groups.join(", ");
-        }
-
-        this.SendMessageToChannel(message);
+        this.CheckSplitSend(
+            all_groups.length <= 0,
+            this.PRE_FORMAT + "No Groups exists yet",
+            this.PRE_FORMAT + "Groups are: " + all_groups.join(", ")
+        );
     }
 
     OnGroupRemove(args : string[], arg_pos : number) : void {
         let group_name = args[arg_pos + 1];
-        let message : string;
 
-        if (!this.group_manager.RemoveGroup(group_name)) {
-            message = this.PRE_FORMAT + "Cannot find group *[" + group_name + "]*";
-        }
-        else {
-            message = this.PRE_FORMAT + "Group *[" + group_name + "]* removed successfully"; 
-        }
-
-        this.SendMessageToChannel(message);
+        this.CheckSplitSend(
+            !this.group_manager.RemoveGroup(group_name),
+            this.PRE_FORMAT + "Cannot find group *[" + group_name + "]*",
+            this.PRE_FORMAT + "Group *[" + group_name + "]* removed successfully"
+        );
     }
 
     OnGroupRename(args : string[], arg_pos : number) : void {
         let old_group_name = args[arg_pos + 1];
         let new_group_name = args[arg_pos + 2];
 
-        let message : string;
-
-        if (!this.group_manager.RenameGroup(old_group_name, new_group_name)) {
-            message = this.PRE_FORMAT + "Cannot find group *[" + old_group_name + "]*";
-        }
-        else {
-            message = this.PRE_FORMAT + "Group *[" + old_group_name + "]* renamed successfully to *[" + new_group_name + "]*"; 
-        }
-
-        this.SendMessageToChannel(message);
+        this.CheckSplitSend(
+            !this.group_manager.RenameGroup(old_group_name, new_group_name),
+            this.PRE_FORMAT + "Cannot find group *[" + old_group_name + "]*",
+            this.PRE_FORMAT + "Group *[" + old_group_name + "]* renamed successfully to *[" + new_group_name + "]*"
+        );
     }
 
 }
